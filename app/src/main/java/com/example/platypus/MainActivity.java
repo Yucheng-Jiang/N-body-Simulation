6ac20a950746b7b4e2e8674e99c5b1266120b8c1
Yucheng Jiang <yj17@illinois.edu>
2019-11-18 10:33:34 -0600

    Initial(2) 11/18/2019

---
+++ app/src/main/java/com/example/platypus/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.platypus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // project starts here
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
