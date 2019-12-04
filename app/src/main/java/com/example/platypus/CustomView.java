4bb3109f73c9808b3ed6e0ef4ccc26679f9f0733
Chengyang Song <csong19@illinois.edu>
2019-12-04 03:50:00 -0600

    12.4 it is movable!

--- app/src/main/java/com/example/platypus/CustomView.java
+++ app/src/main/java/com/example/platypus/CustomView.java
@@ -5,13 +5,18 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
+
 import java.util.Random;
 
 import androidx.annotation.Nullable;
 
 public class CustomView extends View {
     private float scale = 1;
+    private Vector position = new Vector();
+    private Vector mPreviousTouch = new Vector();
 
     public CustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -33,13 +38,13 @@ public class CustomView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.scale(scale, scale);
+        canvas.translate((float) position.getX(), (float) position.getY());
         for (int i = 0; i < Planet.planetList.size(); i++) {
             Planet p = Planet.planetList.get(i);
             Paint paint = new Paint();
-            Random rand = new Random();
             paint.setColor(p.getColor());
 
-
+            canvas.drawPath(p.path, paint);
             canvas.drawCircle(
                     (float) p.getPosition().getX(),
                     (float) p.getPosition().getY(),
@@ -50,7 +55,28 @@ public class CustomView extends View {
         invalidate();
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPreviousTouch.set(event.getX(), event.getY());
+                //Toast.makeText(getContext(), "click", Toast.LENGTH_SHORT).show();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Vector currentTouch = new Vector(event.getX(), event.getY());
+                currentTouch.minus(mPreviousTouch);
+                position.add(currentTouch.getX(), currentTouch.getY());
+                Toast.makeText(getContext(), "move", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        mPreviousTouch.set(event.getX(), event.getY());
+        return true;
+    }
+
     public void setScale(float x) {
         scale = x;
+    }
+    public void setPosition(double x, double y) {
+        position.set(x, y);
     }
 }
